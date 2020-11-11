8773fc85c69cf8ce2862d35ee6c21a3dfb47c960
Tim <tim@unegma.com>
2020-11-11 02:33:41 +0000

    Add function for getting a value from an array of objects or an object

--- index.js
+++ index.js
@@ -15,3 +15,29 @@ module.exports.addDelayToPromises = function (promises, delayIncrement = 1000) {
   console.log(`Finishing addDelayToPromises`);
   return newPromises;
 }
+
+/**
+ * Return Attribute Value
+ *
+ * Return a value from an array of objects, otherwise return undefined (or empty string)
+ * searchData can either be an array of objects or an object.
+ *
+ * @param searchData
+ * @param key
+ * @param useKey
+ * @param returnType
+ * @returns {*|undefined}
+ */
+module.exports.getAttValue = function (searchData, key, useKey = 'machine_name', returnType = undefined) {
+  if (Array.isArray(searchData)) {
+    const attribute = searchData.find(xA => xA[useKey] === key);
+    // returning undefined will allow default parameters to be used when calling functions myVariable = ""
+    return attribute !== undefined ? attribute.value : returnType;
+  } else {
+    if (searchData[useKey] === key) {
+      return searchData.value;
+    } else {
+      return returnType;
+    }
+  }
+}
